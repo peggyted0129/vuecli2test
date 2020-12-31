036064feb0b85030f0ebb93e553c89c8fe43f938
peggyted0129 <peggyted0129@gamil.com>
2020-12-31 12:52:48 +0800

    修正參考路徑

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuecakeshop/',
+    assetsPublicPath: '/vuecli2test/',
 
     /**
      * Source Maps
